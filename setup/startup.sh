f41a69981a1b13d5e661d0c542c0c7fd64608c58
ddesmond <crni.aki@gmail.com>
2025-05-31 09:42:57 +0200

    updates

--- setup/startup.sh
+++ setup/startup.sh
@@ -4,13 +4,9 @@ if [ -e /.comfyui-init ]; then
   bash /setup/setup.sh
   bash /setup/folder_setup.sh
   bash /setup/run_comfyui.sh
-elif [ -z "${REFRESH}" ];
+elif [ -z "${REFRESH}" ]; then
   echo "_____ Refresh _____"
   bash /setup/setup.sh
-  bash /setup/run_comfyui.sh
-elif [ -e /data/models ]; then
-  echo "_____ Reset folders  _____"
-  bash /setup/folder_setup.sh
   bash /setup/run_comfyui.sh
 else
   bash /setup/run_comfyui.sh
